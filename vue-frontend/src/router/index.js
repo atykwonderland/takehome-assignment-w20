02f097108749929202e2123e9623c40813cba017
Alice Kang <atyk1105@gmail.com>
2020-01-26 14:42:36 -0500

    complete all parts, start bonus

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/Home";
+import Counter from "@/components/Counter";
 
 Vue.use(Router);
 
@@ -10,6 +11,8 @@ export default new Router({
       path: "/",
       name: "Home",
       component: Home
-    }
-  ] // Add a new route here in Part 2
-});
+    },
+    //Part 2: new route to counter
+    {                                                                                     path: "/counter",                                                                   name: "Counter",                                                                    component: Counter                                                                } 
+  ] 
+}); 
